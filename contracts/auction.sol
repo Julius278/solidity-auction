8c695ad8fece2b0c949edfb93009bfbdcd87bb67
Remix Workspace <>
2022-08-17 16:38:46 +0200

    implemented the placeBid function and added a min function

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -27,64 +27,74 @@ contract Auction{
         require(msg.sender == owner);
         _;
     }
-
     modifier notOwner(){
         require(msg.sender != owner);
         _;
     }
-
-    modifier auctionRunning(){
-        require(auctionIsRunning() == State.RUNNING);
+    modifier afterStart(){
+        require(block.number >= startBlock);
+        _;
+    }
+    modifier beforeEnd(){
+        require(block.number <= endBlock);
         _;
     }
 
-    modifier auctionNotRunning(){
-        require(auctionIsRunning() != State.RUNNING);
-        _;
-    }
-
-    receive() external payable notOwner auctionRunning {
-        addBid(msg.sender, msg.value);
+    receive() external payable notOwner afterStart beforeEnd {
+        //addBid(msg.sender, msg.value);
     }
 
     fallback() external payable{
         revert();
     }
 
-    function addBid(address addr, uint bid) internal{
-        //add new address or add increment bid to mapping
+    function placeBid() internal notOwner afterStart beforeEnd{
+        require(getAuctionState() == State.RUNNING);
+        require(msg.value >= bidIncrement);
+
+        uint currentBid = bids[msg.sender] + msg.value;
+        require(currentBid > highestBindingBid);
+        bids[msg.sender] = currentBid;
+
+        if(currentBid <= bids[highestBidder]){
+            highestBindingBid = min(currentBid + bidIncrement, bids[highestBidder]);        
+        } else {
+            highestBindingBid = min(currentBid, bids[highestBidder] + bidIncrement);
+            highestBidder = payable(msg.sender);
+        }
+    }
+
+    function min(uint a, uint b) pure internal returns(uint){
+        if(a <= b){
+            return a;
+        }
+        return b;
     }
 
     //auction is running and bidders can add bids
-    function auctionIsRunning() public view returns(State){
+    function getAuctionState() public view returns(State){
         return auctionStatus;
     }
 
-
-    function getHighestBidder() public view returns(address){
-
-    }
-
-    function getHighestBindingBid() public view returns(uint){
-
-    }
-
-    function startAuction() public onlyOwner auctionNotRunning {
+    function startAuction() public onlyOwner afterStart beforeEnd {
         auctionStatus = State.RUNNING;
 
         startBlock = block.number;
         // approx. one week, 15 seconds per new Block, 40320 new Blocks
         endBlock = startBlock + 40320;
-
         //clear bids
     }
 
-    function endAuction() public onlyOwner auctionRunning {        
+    function endAuction() public onlyOwner afterStart beforeEnd {        
         auctionStatus = State.ENDED;
 
         //get Highest Bidder
         //withdraw highest bid to owner
         //withdraw open funds to bidders
         //clear bids
+        //clear highestBindingBid
+        highestBindingBid = 0;
+        //clear highestBidder
+        highestBidder= payable(address(0));
     }
 }
